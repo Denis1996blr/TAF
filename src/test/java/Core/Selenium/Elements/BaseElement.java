9b0a96bac692c7d5374d2c262ea3ac1d07f0c335
Denis Antonenko <dantonenko@aras.com>
2020-06-08 00:11:11 +0300

    Add implementation of wrappers for Selenium elements.

---
+++ src/test/java/Core/Selenium/Elements/BaseElement.java
@@ -0,0 +1,74 @@
+package Core.Selenium.Elements;
+
+import Core.Selenium.BaseEntity;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class BaseElement extends BaseEntity {
+
+    private final By locator;
+    protected WebElement element;
+
+    public BaseElement(By locator) {
+        this.locator = locator;
+    }
+
+    public By getLocator() {
+        return locator;
+    }
+
+    public WebElement getElement() {
+        if (locator != null) {
+            try {
+                element = browser.driver.findElement(locator);
+            } catch (Exception e) {
+                return null;
+            }
+        }
+        return element;
+    }
+
+    public Boolean isVisible() {
+        try {
+            return getElement().isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public Boolean isEnabled() {
+        waitElementIsVisible();
+        return getElement().isEnabled();
+    }
+
+    public Boolean isPresent() {
+        return getElement() != null;
+    }
+
+    public void click() {
+        waitElementIsVisible();
+        element.click();
+    }
+
+    public String getAttribute(String attr) {
+        waitElementIsVisible();
+
+        return element.getAttribute(attr);
+    }
+
+    public BaseElement waitElementIsVisible() {
+        waitForCondition(expectedCondition -> isVisible(), String.format("Element with locator '%s' isn't visible.", this.locator));
+        return this;
+    }
+
+    public BaseElement waitElementIsPresent() {
+        waitForCondition(expectedCondition -> isPresent(), String.format("Element with locator '%s' isn't present.", this.locator));
+        return this;
+    }
+
+    public String getText()
+    {
+        waitElementIsVisible();
+        return getElement().getText();
+    }
+}
---
+++ src/test/java/Core/Selenium/Elements/Element.java
@@ -0,0 +1,10 @@
+package Core.Selenium.Elements;
+
+import org.openqa.selenium.By;
+
+public class Element extends BaseElement {
+
+    public Element(By locator) {
+        super(locator);
+    }
+}
